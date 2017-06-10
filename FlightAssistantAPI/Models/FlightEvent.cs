1ade90664bc423b4a4c85435ce6349e4166c328c
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2017-06-10 03:34:35 +0200

    gate

--- FlightAssistantAPI/Models/FlightEvent.cs
+++ FlightAssistantAPI/Models/FlightEvent.cs
@@ -21,7 +21,7 @@ namespace FlightAssistantAPI.Models
                 switch (Type)
                 {
                     case FlightEventType.Boarding:
-                        return "Boarding GATE" + BoardingGate;
+                        return "Boarding at GATE " + BoardingGate;
                     case FlightEventType.CheckInOnline:
                         return "Checkin online";
                     case FlightEventType.CheckInOffline:
